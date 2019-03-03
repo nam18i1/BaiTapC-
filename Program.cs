f99838eb878db5bf092fc2f36f8e27f6bb43de3b
nam18i1 <48077295+nam18i1@users.noreply.github.com>
2019-03-03 17:20:57 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CauA TT = new CauA();
+            TT.nhapN();
+            TT.InTong();
+            Console.ReadLine();
+        }
+    }
+}
