a262a35a781b594c37ac3e79cedfe61563a1154b
nam18i1 <48077295+nam18i1@users.noreply.github.com>
2019-03-22 12:12:20 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THConStructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Nhap so SV:");
+            n = Int32.Parse(Console.ReadLine());
+            SinhVien[] sv = new SinhVien[n];
+            for(int i = 0; i < n; i++)
+            {
+                sv[i] = new SinhVien();
+                sv[i].nhap();
+                sv[i].IN();
+            }
+            float min = sv[0].TB();
+            for(int i = 1; i < n; i++)
+            {
+                if (sv[i].TB() < min)
+                    min = sv[i].TB();
+             }
+            Console.WriteLine("Sinh vien co diem TB thap nhat la: {0}", min);
+            for(int i =0; i < n; i++)
+            {
+                if (sv[i].masv.Contains("IT"))
+                    Console.WriteLine("Ma sv {0} co diem TB la: {1}", sv[i].masv, sv[i].TB());
+            }
+            for(int i = 0; i < n; i++)
+            {
+                for(int j=i+1; j< n;j++)
+                {
+                    if(sv[j].TB() < sv[i].TB())
+                   {       
+                       SinhVien t =  new SinhVien();
+                        t = sv[i];
+                        sv[i] = sv[j];
+                        sv[j] = t;
+                    }
+                }
+             }
+            Console.WriteLine("Diem TB sap xep theo thu tu tang dan:");
+            for(int i = 0; i  < n; i++)
+                Console.WriteLine("MaSv: {0} DiemTB: {1}",sv[i].masv,sv[i].TB());
+            Console.ReadLine();
+        }
+    }
+}
+
+
---
+++ SinhVien.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THConStructor
+{
+    class SinhVien
+    {
+        public String masv;
+        float toan;
+        float ly;
+        float hoa;
+        public SinhVien()
+        {
+            this.masv = "01";
+            this.toan = (float)7.5;
+            this.ly = (float)8.5;
+            this.hoa = (float)6.0;
+        }
+        public SinhVien(String ma, float toan, float ly, float hoa)
+        {
+            this.masv = ma;
+            this.toan = toan;
+            this.ly = ly;
+            this.hoa =hoa;
+        }
+        public void nhap()
+        {
+            Console.WriteLine("Nhap Masv:");
+            this.masv = Console.ReadLine();
+            Console.WriteLine("Nhap diem mon toan");
+            this.toan = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap diem mon ly");
+            this.ly = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap diem mon hoan");
+            this.hoa = float.Parse(Console.ReadLine());
+        }
+        public float TB()
+        {
+            float tb = (this.toan + this.ly + this.hoa) / 3;
+            return tb;
+        }
+        public void IN()
+        {
+            Console.WriteLine("MaSV {0}, Toan: {1}, Ly: {2}, Hoa: {3}", this.masv, this.toan, this.ly, this.hoa);
+            Console.WriteLine("Diem TB la: {0}", this.TB());
+        }
+    }
+}
