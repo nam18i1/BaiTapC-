aeb6766bed0ae0f1e6c60be8f779e913aa80ec40
nam18i1 <48077295+nam18i1@users.noreply.github.com>
2019-04-04 11:12:38 +0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace THWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void TongMuX_Click(object sender, RoutedEventArgs e)
+        {
+            int n = Int32.Parse(txtsoN.Text);
+            double s = 0;
+            int x = 2;
+            for (int i = 1; i <= n; i++)
+            {
+                s += Math.Pow(x, i);
+            }
+            txtkq.Text = s.ToString();
+
+        }
+
+        private void BtGT_Click(object sender, RoutedEventArgs e)
+        {
+            int n = Int32.Parse(txtSoN.Text);
+            double s = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                s += Math.Sqrt(i);
+            }
+            txtKQ.Text = s.ToString();
+        }
+
+        private void Min_Click(object sender, RoutedEventArgs e)
+        {
+            int[] m;
+            int n = Int32.Parse(textSoMang.Text);
+        }
+    }
+}
